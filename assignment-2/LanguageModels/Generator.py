d8c6c900d4c89e0c4e6f52dee819b549b4a2820e
Calradia <63313283+Calradia@users.noreply.github.com>
2020-11-08 23:04:31 +0100

    Update Generator.py

--- assignment-2/LanguageModels/Generator.py
+++ assignment-2/LanguageModels/Generator.py
@@ -3,8 +3,7 @@ import argparse
 import codecs
 from collections import defaultdict
 import random
-import numpy # For selecting words based on their probabilities
-
+import numpy
 """
 This file is part of the computer assignments for the course DD1418/DD2418 Language engineering at KTH.
 Created 2018 by Johan Boye and Patrik Jonell.
@@ -15,8 +14,8 @@ class Generator(object) :
     This class generates words from a language model.
     """
     def __init__(self):
-
-        # The mapping from words to identifiers.
+    
+        # The mapping from words to identifiers. 
         self.index = {}
 
         # The mapping from identifiers to words.
@@ -65,17 +64,21 @@ class Generator(object) :
         try:
             with codecs.open(filename, 'r', 'utf-8') as f:
                 self.unique_words, self.total_words = map(int, f.readline().strip().split(' '))
-                # REUSE YOUR CODE FROM BigramTester.py here
-                for i in range(self.unique_words):
-                    _, word, frequency = map(str, f.readline().strip().split(' '))
-                    self.index[i], self.word[word], self.unigram_count[word] = word, i, int(frequency)
-                for line in f:
-                    if line.strip() == "-1":
-                        break
-                    # Get index of first word and second word respectively, and their bigram prob
-                    i, j, prob = map(str, line.strip().split(' '))
-                    first_word, second_word = self.index[int(i)], self.index[int(j)]
-                    self.bigram_prob[int(i)][int(j)], self.bigram_prob[first_word][second_word] = float(prob), float(prob)
+                i = 0
+                while True:
+                    if i<self.unique_words:
+
+                        row = f.readline().strip().split(' ')
+                        self.index[row[1]]=row[0]
+                        self.word[row[0]]=row[1]
+                        self.unigram_count[row[0]]=int(row[2])/self.total_words
+                        i += 1
+                    else:
+                        row = f.readline().strip().split(' ')
+                        if int(row[0])== -1:
+                            break
+
+                        self.bigram_prob[row[0]][row[1]] = row[2]
                 return True
         except IOError:
             print("Couldn't find bigram probabilities file {}".format(filename))
@@ -85,55 +88,63 @@ class Generator(object) :
         """
         Generates and prints n words, starting with the word w, and following the distribution
         of the language model.
-        """
-        # YOUR CODE HERE
+        """ 
+
+
+        self.last_index = self.index[w]
         words_to_print = []
-        words_to_print.append(w) # First word (user-inputted one) added
+        words_to_print.append(w)
 
         for i in range(n):
-            words = []
-            probabilities = []
-            most_recent_word = words_to_print[len(words_to_print) - 1] # Index of most recently used word (starts with starting word)
-            subsequent_words = self.bigram_prob[most_recent_word] # All words that ever appear after the first word, and the probabilities
-            for next_word in subsequent_words:
-                # For each word that follows
-                words.append(next_word) # The word
-                log_prob = subsequent_words[next_word] # Get that bigram's log probability
-                #regular_prob = math.exp(log_prob) # The bigram's (most_recent_word + next_word) probability (NOT expressed as a log)
-                #regular_prob = float("{0:.15f}".format(regular_prob))
-                probabilities.append(math.exp(log_prob))
-            if words:
-                # If the bigram was also in our language model
-                ## Received error "probabilities do not sum to one" from numpy. Fixed it by following link below:
-                ## https://stackoverflow.com/questions/46539431/np-random-choice-probabilities-do-not-sum-to-1
-                p = numpy.array(probabilities)
-                p /= p.sum() # Normalize
-                chosen_word = numpy.random.choice(words, 1, p=p) # Choose a word
-                words_to_print.append(chosen_word[0])
+            rand = random.random()
+            prob_lim = 0
+
+            if self.last_index != -1 or self.bigram_prob[self.last_index] == {}:
+                for word in self.bigram_prob[self.last_index].keys():
+                    log_prob = float(self.bigram_prob[self.last_index][word])
+                    prob_lim += numpy.exp(float(log_prob))
+                    if rand <= prob_lim:
+                        words_to_print.append(self.word[word])
+                        self.last_index = word
+                        break
             else:
-                # If there were no bigrams, then
-                # we must choose another 'starting' word
-                random_index = random.randrange(0, self.unique_words, 1)
-                new_word = self.index[random_index]
-                words_to_print.append(new_word)
+                for word in self.unigram_count.keys():
+                    prob_lim += self.unigram_count[word]
+                    if rand <= prob_lim:
+                        words_to_print.append(self.word[word])
+                        self.last_index = word
+                        break
 
         for word in words_to_print:
             print(word + ' ')
+        #print(words_to_print)
+        #print(len(words_to_print))
+
+
 
 def main():
     """
     Parse command line arguments
     """
-    parser = argparse.ArgumentParser(description='BigramTester')
-    parser.add_argument('--file', '-f', type=str,  required=True, help='file with language model')
-    parser.add_argument('--start', '-s', type=str, required=True, help='starting word')
-    parser.add_argument('--number_of_words', '-n', type=int, default=100)
+    
 
-    arguments = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='BigramTester')
+    # parser.add_argument('--file', '-f', type=str,  required=True, help='file with language model')
+    # parser.add_argument('--start', '-s', type=str, required=True, help='starting word')
+    # parser.add_argument('--number_of_words', '-n', type=int, default=100)
+
+    # arguments = parser.parse_args()
+
+    # generator = Generator()
+    # generator.read_model(arguments.file)
+    # generator.generate(arguments.start,arguments.number_of_words)
+    
+
 
     generator = Generator()
-    generator.read_model(arguments.file)
-    generator.generate(arguments.start,arguments.number_of_words)
+    generator.read_model("kafka_model.txt")
+    generator.generate("franz",50)
+    
 
 if __name__ == "__main__":
     main()
